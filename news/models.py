214feb4ff2702484ac45bfe76c5836f88e754612
Muhammad <muhammad14toxtasinov@gmail.com>
2025-06-16 07:50:31 +0500

    Some changes

--- news/models.py
+++ news/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+
+class Author(models.Model):
+    fname = models.CharField(max_length=255)
+    lname = models.CharField(max_length=255)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.fname + " " + self.lname
